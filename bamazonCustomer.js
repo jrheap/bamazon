833d306759a6a7a99d89921013c62d0c3c5bda40
Jason <jasonrheap@gmial.com>
2018-07-14 23:20:48 -0600

    working code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -44,37 +44,52 @@ con.query('SELECT * FROM products', function (err, res) {
                 // the function lets you choose what item to buy
                 choices: function(){
                     return inventory.map(function(item){
-                        return "" + item.id + " " + item.product_name + ' ' +  "$" + item.price
+                        return "" + item.id + " " + item.product_name + ' at ' +  "$" + item.price + ' dollars'
                         //return item.id + '|' + item.product_name + '|' + item.department_name + '|' + item.price + '|' + item.stock_quanity; 
-                    });
-                    
+                    });                    
                 }
             }
         ]).then(function (item){
             var itemId = parseInt(item.idBuy.slice(0, 1))
             // console.log(itemId, 'this is item id');
-
             // console.log(item);
              inquirer.prompt([{
                      type: 'input',
                      message: 'How many ' + item.idBuy + 's would you like to buy? There is ' + getChosenItemQty(item) + ' in stock',
                      name: 'amountBuy'
                  }]).then(function (amount) {
-                    console.log(amount.amountBuy);
+                     console.log('\n__________________________________________________________')
+                    // console.log(amount.amountBuy);
                     //if the ammount the user chose is less then the amount we have in stock
-                    console.log(inventory[itemId -1].stock_quanity);
+                    // console.log(inventory[itemId -1].stock_quanity);
                     if (parseInt(amount.amountBuy) > inventory[itemId-1].stock_quanity) {
                         //show nice msg saying we do not have enough of that item
                         console.log("We apologize for the inconvience but we dont have enough stock to fulfil your order");
-                        con.end();
-                    }else{
-                        //sebtract the stock wantity from the amount they wanted to buy
+                        console.log('\n___________________________________________________')
+                        runAgain()
+                        function runAgain(){
+                        inquirer.prompt([{
+                            type: 'confirm',
+                            message: 'We only have have ' + inventory[itemId-1].stock_quanity + ' in stock. Would you like to change your order?',
+                            name: 'answer',
+                        }]).then(function(guess){
+                            console.log(guess)
+                            if (guess.answer === true){
+                                runQ();
+                            }else if (guess.answer === false){
+                                console.log('We hope to see you again soon')
+                                
+                            }})}con.end()}
+                    else{
+                        //subtract the stock wantity from the amount they wanted to buy
                         var newStockQty = (inventory[itemId - 1].stock_quanity - (parseInt(amount.amountBuy)));
                         //we then need to go update our database to reflect that change
-                        console.log(newStockQty, "this is the new stock quantity");
+                        console.log('There are now ' + newStockQty + ' available in stock. ');
                         //else process the transaction
                         con.query('UPDATE products SET stock_quanity = ? WHERE id = ?', [newStockQty, itemId], function (err, updatedData) {
-                            
+                         var total = (inventory[itemId - 1].price * (parseInt(amount.amountBuy)));
+                        
+                         console.log('Your total cost comes to ' + total + ' dollars!');
                     
 
                             con.end();
@@ -82,6 +97,7 @@ con.query('SELECT * FROM products', function (err, res) {
 
                     }
              })
+             console.log(inventory[itemId - 1].price )
 
             //get the chosen item that the user selected
             function getChosenItemQty(item){
